3cf363c44504c8589aafbf1477715194a59c64cb
yasamin-s96 <yasamin.s96@gmail.com>
2023-08-31 20:40:35 +0330

    added new features

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -5,10 +5,11 @@ const Register = () => {
         <Container>
             <Card>
                 <form className="w-full flex flex-col items-center justify-center space-y-3">
-                    <input className="w-full py-2" type="text" placeholder="Firstname" />
-                    <input className="w-full py-2" type="text" placeholder="Lastname" />
-                    <input className="w-full py-2" type="text" placeholder="Username" />
-                    <input className="w-full py-2" type="email" placeholder="Email" />
+                    <input className="w-full py-2 focus:outline-none rounded-md border border-stone-200 px-2 focus:ring-2" type="text" placeholder="Firstname" />
+                    <input className="w-full py-2 focus:outline-none rounded-md border border-stone-200 px-2 focus:ring-2" type="text" placeholder="Lastname" />
+                    <input className="w-full py-2 focus:outline-none rounded-md border border-stone-200 px-2 focus:ring-2" type="text" placeholder="Username" />
+                    <input className="w-full py-2 focus:outline-none rounded-md border border-stone-200 px-2 focus:ring-2" type="email" placeholder="Email" />
+                    <input className="block w-full bg-indigo-300 rounded-lg p-2" type="submit" value={"Sign Up"} />
                 </form>
             </Card>
         </Container>
